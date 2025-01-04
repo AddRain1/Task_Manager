d082d1edafa890e2cc04849499b2e6910f832c57
AddRain1 <77909535+AddRain1@users.noreply.github.com>
2025-01-03 23:36:00 -0800

    create routes and set up db

---
+++ backend/app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks')
+
+// middleware
+app.use(express.json());
+
+// routes
+app.get('/Hello', (req, res) => {
+    res.send('Hello World');
+});
+
+app.use('/api/v1/tasks', tasks)
+
+// app.get('/api/v1/tasks') get all tasks
+// app.post('/api/v1/tasks') create a new task
+// app.get('/api/v1/tasks/:id') get a single task
+// app.patch('/api/v1/tasks/:id') update a task
+// app.delete('/api/v1/tasks/:id') delete a task
+
+const port = 3000;
+
+app.listen(port, console.log(`Server is running on port ${port}...`));
---
+++ backend/db/connect.js
@@ -0,0 +1,5 @@
+const mongoose = require('mongoose')
+
+const connectionString = 'mongodb+srv://adriankvo22:clfiAeg7qSbFEesz@task-manager.8ub7u.mongodb.net/'
+
+mongoose.connect(connectionString,)
